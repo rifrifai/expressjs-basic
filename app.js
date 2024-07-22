243ffd8851ff8a4cc02bcf7f924e1c799e1c4c05
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-07-22 22:02:31 +0700

    learn route

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// routing
+// endpoint bisa sama, asalkan method nya berbeda
+app.get("/api/v1/categories", (req, res) => {
+  res.send("Hello World GET");
+});
+
+app.post("/api/v1/categories", (req, res) => {
+  res.send("Hello From POST");
+});
+
+// server
 app.listen(port, () => {
   console.info(`Server is running on port ${port}`);
 });
