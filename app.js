1f7befe89ceec0f2aad83e40a963e79a695e555a
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-07-22 22:30:59 +0700

    response json

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 const CategoriesRouter = require("./routes/categories");
 
+// middleware
+app.use(express.json());
+
 // routing
 app.use("/api/v1/categories", CategoriesRouter);
 
--- routes/categories.js
+++ routes/categories.js
@@ -3,8 +3,21 @@ const router = express.Router();
 
 // routing
 // endpoint bisa sama, asalkan method nya berbeda
+// /api/v1/categories
 router.get("/", (req, res) => {
-  res.send("Hello World GET");
+  res.json({
+    message: "Hello World GET",
+    data: [
+      {
+        id: 1,
+        name: "IPhone",
+      },
+      {
+        id: 2,
+        name: "Samsung",
+      },
+    ],
+  });
 });
 
 router.post("/", (req, res) => {
@@ -12,10 +25,12 @@ router.post("/", (req, res) => {
 });
 
 // kalau ada tambahan di letakan di atas :nama
+// /api/v1/categories/filterData
 router.get("/filterData", (req, res) => {
   res.send("Route Filter Data");
 });
 
+// /api/v1/categories/:nama
 router.get("/:nama", (req, res) => {
   res.send(`Endpoint from route params ${req.params.nama}`);
 });
