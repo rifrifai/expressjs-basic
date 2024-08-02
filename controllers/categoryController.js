c104a0fa61da5e0e7e84eb34ba077c6aee51820a
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-02 09:33:34 +0700

    enhanced object literal

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,3 +1,5 @@
+const { category } = require("../models/category");
+
 exports.getAllCategories = (req, res) => {
   // console.info(req.reqTime);
   res.status(200).json({
@@ -20,12 +22,19 @@ exports.getAllCategories = (req, res) => {
 };
 
 // membuat validasi
-exports.storeCategory = (req, res) => {
+exports.storeCategory = async (req, res) => {
   // let name = req.body.name;
   // let description = req.body.description;
 
   try {
     let { name, description } = req.body;
+    const newCategory = await Category.create({
+      name: name,
+      description: description,
+      // bisa sekali pemanggilan asal namanya sama dengan enhanced object literals
+      // name
+      // description
+    });
   } catch (error) {
     return res.status(400).json({
       status: "Fail",
