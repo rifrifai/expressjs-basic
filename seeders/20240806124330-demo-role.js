3a1eab8819b0ceba1f0b3c6d8f11d66ba18a535a
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-06 20:03:20 +0700

    run seeder success

--- seeders/20240806124330-demo-role.js
+++ seeders/20240806124330-demo-role.js
@@ -1,8 +1,9 @@
-'use strict';
+"use strict";
+const { v4 } = require("uuid");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /**
      * Add seed commands here.
      *
@@ -11,15 +12,26 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
+    return queryInterface.bulkInsert("Roles", [
+      {
+        id: v4(),
+        name: "siswa",
+      },
+      {
+        id: v4(),
+        name: "admin",
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+    return queryInterface.bulkDelete("Roles");
+  },
 };
