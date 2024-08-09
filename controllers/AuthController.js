53d22108e2732c7c3f9210bf3d0ebfa466a34c62
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-09 22:18:52 +0700

    refactory autentikasi

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -2,18 +2,20 @@ const { Siswa } = require("../models");
 
 exports.registerSiswa = async (req, res) => {
   try {
-    let { name, email, password, passwordConfirm } = req.body;
+    // jangan menggunakan variable saat signup or logging in because some attackers can access tha variable
+    // let { name, email, password, passwordConfirm } = req.body;
 
-    if (password != passwordConfirm) {
+    if (req.body.password != req.body.passwordConfirm) {
       return res.status(400).json({
-        message: "Password and Password Confirmation do not match",
+        message: "Validation Error",
+        error: "Password and Password Confirmation do not match",
       });
     }
 
     const newSiswa = await Siswa.create({
-      name,
-      email,
-      password,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
     });
 
     return res.status(200).json({
@@ -24,7 +26,7 @@ exports.registerSiswa = async (req, res) => {
     console.info(error);
     return res.status(400).json({
       message: "Validation Error",
-      error,
+      error: error.errors.map((err) => err.message),
     });
   }
 };
