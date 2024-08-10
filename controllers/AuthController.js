20b4c3449b772c4cadf2d43c1421e9b654c1752a
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-10 21:52:27 +0700

    login with token authentication

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,4 +1,11 @@
 const { Siswa } = require("../models");
+const jwt = require("jsonwebtoken");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
 
 exports.registerSiswa = async (req, res) => {
   try {
@@ -18,8 +25,11 @@ exports.registerSiswa = async (req, res) => {
       password: req.body.password,
     });
 
+    const token = signToken(newSiswa.id);
+
     return res.status(200).json({
       status: "Register Success",
+      token,
       data: newSiswa,
     });
   } catch (error) {
@@ -32,5 +42,29 @@ exports.registerSiswa = async (req, res) => {
 };
 
 exports.loginSiswa = async (req, res) => {
-  res.send("Endpoint Login Success");
+  // 1-fungsi validasi
+  if (!req.body.email || !req.body.password) {
+    return res.send(400).json({
+      status: "Fail",
+      message: "Validation Error",
+      error: "email and password are required",
+    });
+  }
+
+  // 2-cek jika email siswa yang di masukan di req sudah ada di database dan password sudah sesuai
+  const siswaData = await Siswa.findOne({ where: { email: req.body.email } });
+
+  if (
+    !siswaData ||
+    !(await siswaData.CorrectPassword(req.body.password, siswaData.password))
+  ) {
+    return res.status(400).json({
+      status: "Fail",
+      message: "Login Failed",
+      error: "Invalid Email or Password",
+    });
+  }
+
+  // 3-token di res pada login
+  const token = signToken(siswaData.id);
 };
